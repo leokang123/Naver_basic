51fd939c00fe7bd38983a9d20f10d48a524b788f
leokang123 <leokang123@github.com>
2024-07-16 20:51:15 +0900

    merge conflict complete

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 const { add, multi, sub } = require("./test");
-=======
 const { add, multi } = require("./test");
 const { printHello, printBye } = require("./test2");
->>>>>>> main4
 
 
 console.log(add(1,2));
 
-<<<<<<< HEAD
 console.log(sub(1,2));
 
 console.log(multi(2,3));
-=======
 printHello();
 
 printBye();
->>>>>>> main4
