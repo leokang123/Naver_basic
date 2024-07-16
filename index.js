50e18797656ba00d5f7e614ac64c6677763fef0a
leokang123 <leokang123@github.com>
2024-07-16 20:37:47 +0900

    Add: function
    - printHello: prints hello
    - printBye: prints bye

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const { add, multi } = require("./test");
+const { printHello, printBye } = require("./test2");
 
 
 console.log(add(1,2));
+
+printHello();
+
+printBye();
---
+++ test2.js
@@ -0,0 +1,12 @@
+function printHello() {
+  console.log("Hello");
+}
+
+function printBye() {
+  console.log("Bye");
+}
+
+module.exports = {
+  printBye,
+  printHello
+};
